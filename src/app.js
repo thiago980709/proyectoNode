8a0d88ddb42119e95544125f073d136b0881bc08
thiago980709 <santixdim100@gmail.com>
2019-06-11 21:40:48 -0500

    arreglos

---
+++ src/app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser')
+const dirNode_modules = path.join(__dirname , '../node_modules')
+const funciones = require('./funciones');
+
+
+
+const directoriopublico = path.join(__dirname,'../public');
+const directoriopartials = path.join(__dirname,'../partials');
+app.use(express.static(directoriopublico));
+hbs.registerPartials(directoriopartials);
+app.use(bodyParser.urlencoded({extended: false}));
+//Bootstrap
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+app.set('view engine' , 'hbs');
+
+
+
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+
+app.get('/crearCurso',(req, res)=>{
+    res.render('crearCurso',{
+        est: req.body.nombre
+    });
+});
+app.get('/login',(req, res)=>{
+    res.render('login');
+});
+app.get('/inscribir',(req, res)=>{
+    res.render('crearCurso');
+});
+app.get('/registro',(req, res)=>{
+    res.render('registro');
+});
+app.post('/indexU',(req, res)=>{
+    
+    if(funciones.crear(req.body) == false){
+        res.render('registro',{
+            err:"Este estudiante ya esta registrado"
+        });
+    }else{
+        res.render('indexU',{
+            est:req.body.nombre
+        });
+    }
+    
+});
+
+app.post('/curso',(req,res)=>{
+    if(funciones.crearCurso(req.body)==false){
+        res.render('crearCurso',{
+            err:"Este curso ya existe!"
+        });
+    }else{
+        res.render('crearCurso',{
+            err:"Curso registrado con exito"
+        });
+    }
+})
+app.post('/in',(req,res)=>{
+    console.log(req.body);
+    if(req.body.documento == 123456){
+        res.render('indexC',{
+            est: req.body.nombre
+        });
+    }else{
+        res.render('indexU',{
+            est: req.body.nombre
+        });
+    }
+    
+});
+
+app.get('/verCursos',(req, res)=>{
+    res.render('verCursos');
+    
+});
+console.log(__dirname);
+
+app.listen(3000,()=>{
+    console.log('Puero 3000');
+});
---
+++ src/funciones.js
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const hbs = require('hbs');
+listU = [];
+listC =[];
+
+function crear(estudiante){
+    listar();
+    let est = {
+        cc: estudiante.documento,
+        nombre: estudiante.nombre,
+        email: estudiante.email,
+        telfono: estudiante.telefono_cel,
+        tipo:"e"
+
+    }
+    let duplicado = listU.find(nom => nom.cc == estudiante.documento);
+    if(!duplicado){
+        listU.push(est);
+        console.log(listU);
+        guardar();
+    }else{
+        return false;
+    }
+    
+}
+
+const listar = () =>{
+    try{
+        listU = require('../usuarios');
+    }catch(error){
+        listU = [];
+    }
+    
+}
+const guardar = () =>{
+    let datos = JSON.stringify(listU);
+    fs.writeFile('usuarios.json',datos,(err)=>{
+        if(err) throw (err);
+        console.log('Archivo creado con exito');
+    })
+}
+
+const actualizar = (nom, asignatura, calificacion ) =>{
+    listar();
+
+    let encontrado = listE.find(buscar => buscar.nombre == nom );
+
+    if(!encontrado){
+        console.log('El estudiante no existe');
+    }else{
+        encontrado[asignatura] = calificacion;
+        guardar();
+    }
+}
+
+const eliminar = (nom)=>{
+    listar();
+    let nuevo = listE.filter(n => n.nombre != nom);
+    if(nuevo.length == listE.length){
+        console.log('Ningun estudinate tiene ese nombre');
+    }else{
+        listE = nuevo;
+        guardar();
+
+    }
+}
+
+/////////////////Cursos
+
+const crearCurso =(curso)=>{
+    listarCurso();
+    let cur = {
+        id:parseInt(curso.newId),
+        nombre: curso.newNombre,
+        des: curso.newDescripcion,
+        valor: parseInt(curso.newValor),
+        modalidad:curso.newModalidad,
+        horas:curso.newHoras,
+        estado:"disponible"
+
+    }
+    let duplicado = listC.find(c => c.id == curso.newId);
+    if(!duplicado){
+        listC.push(cur);
+        console.log(listU);
+        guardarCurso();
+    }else{
+        return false;
+    }
+}
+
+
+const listarCurso = () =>{
+    try{
+        listC = require('../cursos');
+    }catch(error){
+        listC = [];
+    }
+    
+}
+const guardarCurso = () =>{
+    let datos = JSON.stringify(listC);
+    fs.writeFile('cursos.json',datos,(err)=>{
+        if(err) throw (err);
+        console.log('Archivo guardado con exito');
+    })
+}
+
+const mostrarCursos = () =>{
+    listarCurso();
+    let texto = "<table >\
+                    <thead>\
+                        <tr>\
+                            <th>ID</th>\
+                            <th>Curso</th>\
+                            <th>Descripción</th>\
+                        </tr>\
+                    </thead>\
+                    <tbody ";
+
+    listC.forEach(curso => {
+        texto = texto + 
+            '<tr> ' +
+            '<td>' + curso.id + '</td>' +
+            '<td> ' + curso.nombre + '</td>' +
+            '<td> ' + curso.des + '</td>'
+    });
+    texto = texto + '</tbody></table>'
+    return texto;
+}
+
+module.exports = {
+    crear,
+    actualizar,
+    eliminar,
+    crearCurso,
+    mostrarCursos
+}
